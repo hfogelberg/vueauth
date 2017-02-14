9988e8fb7ca0a0d1a37e3334a8b3658083d85ec6
Henrik Fogelberg <henrik.fogelberg@gmail.com>
2017-02-14 14:15:36 +0000

    Tweaks

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import VueRouter from 'vue-router';
 import Hello from './components/Hello.vue';
 import Auth from './components/Auth.vue';
 import AuthSuccess from './components/AuthSuccess.vue';
+import Stacks from './components/Stacks.vue'
 
 const checkFromAuth = (to, from, next) => {
   const query = window.location.search.substr(1).split('=');
@@ -20,8 +21,10 @@ const router = new VueRouter({
   mode: 'hash',
   routes: [
     { path: '/', component: Hello, beforeEnter: checkFromAuth },
+    { path: '/hello', component: Hello},
     { path: '/auth', component: Auth },
     { path: '/success', component: AuthSuccess },
+    {path: '/stacks', component: Stacks}
   ],
 });
 router.beforeEach((to, from, next) => {
